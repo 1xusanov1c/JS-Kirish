e0992b6df44f2e2ac103f0593a96e2cde58d7c82
1xusanov1c <azz806@gmail.com>
2024-12-10 19:03:16 +0500

    ishni boshladm

---
+++ Js/main.js
@@ -0,0 +1,147 @@
+// "1-masala"
+// let a =  prompt(`Kvadratning tomonini kiriting `);
+// let P = 4*a;
+
+// document.write(`Perimenti: ${P}`);
+
+
+
+
+// "2-masala"
+// let a = prompt(`Kvadratning tomonini kiriting`);
+// let S = a*a;
+
+// document.write(`Yuzasi: ${S}`);
+
+
+
+
+// "3-masala"
+// let a = +prompt(`Togri torburchakning a tomonini kiriting!`);
+// let b = +prompt(`Togri torburchakning b tomonini kiriting!`);
+
+
+// let S = a * b;
+// let P = 2 * (a+b);
+
+// document.write(`Yuzasi: ${S} <br>`);
+// document.write(`Perimenti: ${P} `);
+
+
+
+
+// "4-masala"
+// let d = prompt(`Aylananing diametrni kiriting! `);
+// const Pi = Math.PI ;
+// let L = Pi * d;
+
+// document.write(`Aylananing uzunligi: ${L}`)
+
+
+
+
+// "5-masala"
+// let a = prompt(`Kubning yon tomonining kiriting!`);
+// let V = a * a * a;
+// let S = 6 * a * a;
+
+// document.write(`Kubning hajimi: ${V} <br>`)
+// document.write(`Kubning sirti: ${S}`)
+
+
+
+
+// "6-masala"
+// let a = +prompt(`Paralelepepidning a tomonining kiriting!`)
+// let b = +prompt(`Paralelepepidning b tomonining kiriting!`)
+// let c = +prompt(`Paralelepepidning c tomonining kiriting!`)
+
+
+// let V = a * b * c;
+// let S = 2 * (a*b+b*c+a*c);
+
+// document.write(`Hajimi: ${V} <br>`)
+// document.write(`Tola sirti: ${S}`)
+
+
+
+
+// "7-masala"
+// let R = prompt(`Doiraning radiusini kiriting!`);
+// let Pi = Math.PI
+// let L = 2 * Pi * R ;
+// let S = Pi * R*R;
+
+// document.write(`Doiraning uzunligi: ${L} <br>`)
+// document.write(`Doiraning yuzasi: ${S}`)
+
+
+
+
+// "8-masala"
+// let a = prompt("a sonini kiriting!");
+// let b = prompt("b sonini kiriting!");
+
+// let A = (a + b) / 2;
+
+
+// document.write(`Orta arifmetigi: ${A}`)
+
+
+
+
+// "9-masala"
+// let a = prompt("a sonini kiriting:");
+// let b = prompt("a sonini kiriting:");
+
+// let G = Math.sqrt(a*b);
+
+// document.write(`Ularning o'rta geometrigi: ${G}`)
+
+
+
+
+// "10-masala"
+// let a = +prompt("Birinchi sonni kiriting:") ;
+// let b = +prompt("Ikkinchi sonni kiriting:") ;
+
+
+// let yigindi = a + b;
+// let kopaytma = a * b;
+// let kvadratA = a ** 2;
+// let kvadratB = b ** 2;
+
+// document.write(`Yig'indi: ${yigindi} <br>`)
+// document.write(`Ko'paytma: ${kopaytma} <br>`)
+// document.write(`kvadratA: ${kvadratA} <br>`)
+// document.write(`kvadratB: ${kvadratB} `)
+
+
+
+
+// "11-masala"
+// let a = +prompt("Birinchi sonni kiriting:")
+// let b = +prompt("IKkinchi sonni kiriting:")
+
+// let yigindi = a + b;
+// let kopaytma = a * b;
+// let modulA = Math.abs(a)
+// let modulB = Math.abs(b)
+
+// document.write(`Yig'indi: ${yigindi} <br>`)
+// document.write(`Ko'paytma: ${kopaytma} <br>`)
+// document.write(`Birinchi soning moduli: ${modulA} <br>`)
+// document.write(`Ikkinchi soning moduli: ${modulB}`)
+
+
+
+// "12-masala"
+// let a = +prompt("To'g'ri uchburchakning birinchi katetini kiriting:");
+// let b = +prompt("To'g'ri uchburchakning ikkinchi katetini kiriting:");
+
+
+// let c = Math.sqrt(a ** 2 + b ** 2);
+// let P = a + b + c;
+
+// document.write(`Gipotenuza ${c} <br>`)
+// document.write(`Perimetr ${P}`)
